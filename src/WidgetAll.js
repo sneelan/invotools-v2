ca8458fa079f8cfabbc11d02e0bb04003c4e6559
Neelakandan S <neelakandan.s@ayatacommerce.com>
2023-11-17 11:13:14 +0530

    drag2

--- src/WidgetAll.js
+++ src/WidgetAll.js
@@ -18,7 +18,7 @@ function ColumnComponent({ column, grid, sm, md, lg, length, columnclass}) {
     return null; // Hide the column
   }
 
-  const isExpanded = column.expandState === 'Yes'; // Check the expandState property
+  const isExpanded = column.toggleExpand === 'yes'; // Check the toggleExpand property
   
   const widgetStyle = {};
   let addClassFixedwidth='';
@@ -59,7 +59,7 @@ function ColumnComponent({ column, grid, sm, md, lg, length, columnclass}) {
 
   const AccordionComponent = (      
         <>
-          {column.showexpand === 'Show' ? (                   
+          {column.toggleStatus === 'active' ? (                   
             <Accordion expanded={expanded} onChange={handleChange} defaultExpanded={isExpanded} 
             classes={{ content: 'p-0 m-0', root: expanded ? 'expanded-accordion' : 'collapsed-accordion' }}>
               <AccordionSummary  expandIcon={<ExpandMoreIcon />} classes={{ content: 'p-0 m-0' }}>  
@@ -70,7 +70,7 @@ function ColumnComponent({ column, grid, sm, md, lg, length, columnclass}) {
               </AccordionDetails>
             </Accordion>                     
           ) : (
-            // Display without Accordion when showexpand is 'No'
+            // Display without Accordion when toggleStatus is 'inactive'
             <div className={`widget make-column-height-same ${widgetClassName} ${addClassFixedwidth}`} style={widgetStyle}>
               {column.title && (<div className='widget-title-border'>
                 <h6 class="widget-title " style={{margin:'0 1em'}}>{column.title}</h6>
@@ -85,7 +85,7 @@ function ColumnComponent({ column, grid, sm, md, lg, length, columnclass}) {
     return (
       <Grid item xs={12} sm={sm} md={md} lg={lg} className={widgetClassName}>{columnclass}{AccordionComponent}</Grid>
     )
-  }else if (column.showexpand === 'Hide' || (column.displayStatus === 'active' && column.showexpand === 'Show')) {
+  }else if (column.toggleStatus === 'inactive' || (column.displayStatus === 'active' && column.toggleStatus === 'active')) {
     return (<
       div className='widget-row'>   
         {AccordionComponent}
@@ -109,8 +109,8 @@ function WidgetAll() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-       //const response = await fetch('https://raw.githubusercontent.com/sneelan/invo-customer-json/main/customer-portal-widget.json');
-       const response = await fetch('/customer-portal-widget.json');
+       const response = await fetch('https://raw.githubusercontent.com/sneelan/invo-customer-json/main/customer-portal-widget.json');
+       //const response = await fetch('/customer-portal-widget.json');
         const data = await response.json();
         setData(data); // Corrected to use setData instead of setJsonData
       } catch (error) {
