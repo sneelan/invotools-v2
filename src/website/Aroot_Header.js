8526c3679f99ad2dd6a3e437f4ba77eb55f8a119
Neelakandan S <sneelan@gmail.com>
2024-03-04 16:49:47 +0530

    changes language March 4  ab

--- src/website/Aroot_Header.js
+++ src/website/Aroot_Header.js
@@ -19,57 +19,61 @@ const Aroot_Header = () => {
     // Reset menu state after 3 seconds when in 'hiding' state
     //useEffect(() => {let timer;if (menuState === 'hiding') {timer = setTimeout(() => {setMenuState('');}, 3000);}return () => clearTimeout(timer);}, [menuState]);
 
-    useEffect(() => {
-      const handleClick = (event) => {
-        const dropdownToggle = document.querySelectorAll('.dropdown-toggle');
-        const dropdownMenus = document.querySelectorAll('.dropdown-menu');
-    
-        let isOutsideDropdown = true;
-    
+    const DropDownMenuClick = (event) => {
+      const dropdownToggle = document.querySelectorAll('.dropdown-toggle');
+      const dropdownMenus = document.querySelectorAll('.dropdown-menu');
+  
+      let isOutsideDropdown = true;
+  
+      dropdownToggle.forEach((toggle) => {
+        if (toggle.contains(event.target)) {
+          isOutsideDropdown = false;
+          toggle.classList.toggle('show'); // Toggle 'show' class on the clicked toggle
+          const dropdownMenu = toggle.nextElementSibling;
+          if (dropdownMenu && dropdownMenu.classList.contains('dropdown-menu')) {
+            dropdownMenu.classList.toggle('show');
+          }
+        }
+      });
+  
+      dropdownMenus.forEach((menu) => {
+        if (menu.contains(event.target)) {
+          isOutsideDropdown = false;
+        }
+      });
+  
+      if (isOutsideDropdown) {
         dropdownToggle.forEach((toggle) => {
-          if (toggle.contains(event.target)) {
-            isOutsideDropdown = false;
-            toggle.classList.toggle('show'); // Toggle 'show' class on the clicked toggle
-            const dropdownMenu = toggle.nextElementSibling;
-            if (dropdownMenu && dropdownMenu.classList.contains('dropdown-menu')) {
-              dropdownMenu.classList.toggle('show');
-            }
-          }
+          toggle.classList.remove('show');
         });
-    
         dropdownMenus.forEach((menu) => {
-          if (menu.contains(event.target)) {
-            isOutsideDropdown = false;
-          }
+          menu.classList.remove('show');
         });
-    
-        if (isOutsideDropdown) {
+      } else {
+        const clickedToggle = event.target.closest('.dropdown-toggle');
+        if (!clickedToggle) {
           dropdownToggle.forEach((toggle) => {
             toggle.classList.remove('show');
           });
           dropdownMenus.forEach((menu) => {
             menu.classList.remove('show');
           });
-        } else {
-          const clickedToggle = event.target.closest('.dropdown-toggle');
-          if (!clickedToggle) {
-            dropdownToggle.forEach((toggle) => {
-              toggle.classList.remove('show');
-            });
-            dropdownMenus.forEach((menu) => {
-              menu.classList.remove('show');
-            });
-          }
         }
-      };
-    
-      document.addEventListener('click', handleClick);
+      }
+    };
+
+    const LinkClicked = (title) => {
+      if (menuState === 'show') {toggleMenu();}
+      document.title = `${title} - Admin Dashboard | Invotools`;
+    };
+
+    useEffect(() => {    
+      document.addEventListener('click', DropDownMenuClick);
       return () => {
-        document.removeEventListener('click', handleClick);
+        document.removeEventListener('click', DropDownMenuClick);
       };
     }, []);
-    const LinkClicked = () => {if (menuState === 'show') {toggleMenu();}};
-    
+
   return (
     <>       
         <header>
@@ -87,29 +91,29 @@ const Aroot_Header = () => {
                     </div>
                     <div className="offcanvas-body">
                       <ul className="navbar-nav justify-content-end flex-grow-1 pe-0">
-                        <li className="nav-item"><NavLink activeClassName="active" onClick={LinkClicked} className="nav-link" to="/website/home">Home</NavLink></li>
-                        <li className="nav-item"><NavLink activeClassName="active" onClick={LinkClicked} className="nav-link" to="/website/faq">FAQ</NavLink></li>
+                        <li className="nav-item"><NavLink activeClassName="active" onClick={() => {LinkClicked('Home');}} className="nav-link" to="/website/home" >Home</NavLink></li>
+                        <li className="nav-item"><NavLink activeClassName="active" onClick={() => {LinkClicked('FAQ');}} className="nav-link" to="/website/faq">FAQ</NavLink></li>
 
-                        <li className="nav-item"><NavLink activeClassName="active" onClick={LinkClicked} className="nav-link" to="/website/tutorials">Tutorials</NavLink></li>
-                        <li className="nav-item"><NavLink activeClassName="active" onClick={LinkClicked} className="nav-link" to="/website/documentation">Documentation</NavLink></li>
-                        <li className="nav-item"><NavLink activeClassName="active" onClick={LinkClicked} className="nav-link" to="/website/support">Support</NavLink></li>
+                        <li className="nav-item"><NavLink activeClassName="active" onClick={() => {LinkClicked('Tutorials');}} className="nav-link" to="/website/tutorials">Tutorials</NavLink></li>
+                        <li className="nav-item"><NavLink activeClassName="active" onClick={() => {LinkClicked('Documentation');}} className="nav-link" to="/website/documentation">Documentation</NavLink></li>
+                        <li className="nav-item"><NavLink activeClassName="active" onClick={() => {LinkClicked('Support');}} className="nav-link" to="/website/support">Support</NavLink></li>
 
                         <li className="nav-item dropdown">
                           <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Dropdown
                           </a>
                           <ul className="dropdown-menu">
-                          <li><Link className="dropdown-item" onClick={LinkClicked} >Post Logout</Link></li>
-                            <li><Link className="dropdown-item" onClick={LinkClicked} >Not Found</Link></li>
-                            <li><Link className="dropdown-item" onClick={LinkClicked} >Access Denied</Link></li>
-                            <li><Link className="dropdown-item" onClick={LinkClicked} >Server Error</Link></li>
-                            <li><Link className="dropdown-item" onClick={LinkClicked} >Maintenance</Link></li>
+                          <li><Link className="dropdown-item" onClick={() => {LinkClicked('Logged Out');}} >Post Logout</Link></li>
+{/*                             <li><Link className="dropdown-item" onClick={() => {LinkClicked('Not Found');}} >Not Found</Link></li>
+                            <li><Link className="dropdown-item" onClick={() => {LinkClicked('Access Denied');}} >Access Denied</Link></li>
+                            <li><Link className="dropdown-item" onClick={() => {LinkClicked('Server Error');}} >Server Error</Link></li>
+                            <li><Link className="dropdown-item" onClick={() => {LinkClicked('Maintenance');}} >Maintenance</Link></li> */}
 
-                            {/* <li><NavLink activeClassName="active" onClick={LinkClicked} className="dropdown-item" to="/website/other/postlogout">Post Logout</NavLink></li>
-                            <li><NavLink activeClassName="active" onClick={LinkClicked} className="dropdown-item" to="/website/other/404">Not Found</NavLink></li>
-                            <li><NavLink activeClassName="active" onClick={LinkClicked} className="dropdown-item" to="/website/other/403">Access Denied</NavLink></li>
-                            <li><NavLink activeClassName="active" onClick={LinkClicked} className="dropdown-item" to="/website/other/500">Server Error</NavLink></li>
-                            <li><NavLink activeClassName="active" onClick={LinkClicked} className="dropdown-item" to="/website/other/503">Maintenance</NavLink></li> */}
+                            <li><NavLink activeClassName="active" onClick={() => {LinkClicked('FAQ');}} className="dropdown-item" to="/website/other/postlogout">Post Logout</NavLink></li>
+                            <li><NavLink activeClassName="active" onClick={() => {LinkClicked('FAQ');}} className="dropdown-item" to="/website/other/404">Not Found</NavLink></li>
+                            <li><NavLink activeClassName="active" onClick={() => {LinkClicked('FAQ');}} className="dropdown-item" to="/website/other/403">Access Denied</NavLink></li>
+                            <li><NavLink activeClassName="active" onClick={() => {LinkClicked('FAQ');}} className="dropdown-item" to="/website/other/500">Server Error</NavLink></li>
+                            <li><NavLink activeClassName="active" onClick={() => {LinkClicked('FAQ');}} className="dropdown-item" to="/website/other/503">Maintenance</NavLink></li>
                           </ul>
                         </li>
                       </ul>
